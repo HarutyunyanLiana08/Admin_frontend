c27c8a88747bfd138d1ce2278c0ce7eada30f016
HarutyunyanLiana08 <harutyunyanliana08@gmail.com>
2023-05-04 08:36:52 +0400

    changed

--- src/ProtectRoute/ProtectRoute.js
+++ src/ProtectRoute/ProtectRoute.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { decodeToken } from "react-jwt";
 
-// const ProtectRoute = ({children})=>{
-//     const token =localStorage.getItem('token')
-//     const decodedToken = decodeToken(token);
-//     console.log(decodedToken)
-//     if(!token || decodedToken?.role === 0){
-//         return <Navigate to='/' />
-//     }
-//     return children
-// }
+const ProtectRoute = ({children})=>{
+    const token =localStorage.getItem('token')
+    const decodedToken = decodeToken(token);
+    console.log(decodedToken)
+    if(!token || decodedToken?.role === 0){
+        return <Navigate to='/' />
+    }
+    return children
+}
 
 export default ProtectRoute
